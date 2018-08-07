03d39afcec889a143dc9e371c47e8da2669cfbe6
KJ Shanks <kyle.j.shanks@gmail.com>
2018-08-06 22:19:19 -0400

    Complete W3D2 Homework

--- W3D2/plays.rb
+++ W3D2/plays.rb
@@ -19,6 +19,27 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    data = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT * FROM plays WHERE title = ?
+    SQL
+    data.map { |hash| Play.new(hash) }
+  end
+
+  def self.find_by_playwright(playwright)
+    data = PlayDBConnection.instance.execute(<<-SQL, playwright)
+      SELECT
+        *
+      FROM
+        plays
+      JOIN
+        playwrights ON plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name = ?
+    SQL
+    data.map { |hash| Play.new(hash) }
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -27,7 +48,7 @@ class Play
   end
 
   def create
-    raise "#{self} already in database" if @id
+    raise "#{self} already exists in database" if @id
     PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id)
       INSERT INTO
         plays (title, year, playwright_id)
@@ -47,5 +68,60 @@ class Play
       WHERE
         id = ?
     SQL
+    true
+  end
+end
+
+class Playwright
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT * FROM playwrights WHERE name = ?
+    SQL
+    data.map { |hash| Playwright.new(hash) }
+  end
+
+  attr_accessor :name, :birth_year
+
+  def initialize(options)
+    @id = options["id"]
+    @name = options["name"]
+    @birth_year = options["birth_year"]
+  end
+
+  def create
+    raise "#{self} already exists in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights(name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+    true
+  end
+
+  def get_plays
+    data = PlayDBConnection.instance.execute(<<-SQL, @id)
+      SELECT * FROM plays WHERE playwright_id = ?
+    SQL
+    data.map { |hash| Play.new(hash)  }
   end
 end

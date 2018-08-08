eececfedc72a07d66fe3346407be5f9e6e141e53
KJ Shanks <kyle.j.shanks@gmail.com>
2018-08-07 21:27:16 -0400

    Add house model and house-person associations

--- W3D3/Test/db/schema.rb
+++ W3D3/Test/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_234733) do
+ActiveRecord::Schema.define(version: 2018_08_08_000820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name", null: false

29a84d84603aa315660b3ec0c1954cf862a4aece
KJ Shanks <kyle.j.shanks@gmail.com>
2018-08-17 08:29:54 -0400

    Update W4D5 Homework

--- W4D5/spec/controllers/users_controller_spec.rb
+++ W4D5/spec/controllers/users_controller_spec.rb
@@ -10,13 +10,22 @@ RSpec.describe UsersController, type: :controller do
 
   describe "POST #create" do
     context "with invalid params" do
-      it "validates the presence of the user's email and password"
+      it "validates the presence of the user's email and password" do
+        post :create, { username: 'Daffy', email: '' }
+        expect(response).to render_template("new")
+      end
 
-      it "validates that the password is at least 6 characters long"
+      it "validates that the password is at least 6 characters long" do
+        post :create, { username: 'Daffy', email: 'daffy@ducks.com', password: 'daffy' }
+        expect(response).to render_template("new")
+      end
     end
 
     context "with valid params" do
-      it "redirects user to bands index on success"
+      it "redirects user to bands index on success" do
+        post :create, { username: 'Daffy', email: 'daffy@ducks.com', password: 'daffyduck' }
+        expect(response).to redirect_to(user_url(User.find_by(username: 'Daffy')))
+      end
     end
   end
 end

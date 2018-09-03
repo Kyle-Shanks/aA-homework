8057df7263ed7ac856791cba30935c32f74e1640
KJ Shanks <kyle.j.shanks@gmail.com>
2018-09-03 19:41:54 -0400

    Add W7D2 Homework

---
+++ W7D2/middleware_intro/frontend/todo_redux.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+// const addLoggingToDispatch = (store) => {
+//   const disp = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState())
+//     console.log(action)
+//     disp(action);
+//     console.log(store.getState())
+//   };
+// };
+
+// const addLoggingToDispatch = store => next => action => {
+//   console.log(store.getState());
+//   console.log(action);
+//
+//   let result = next(action);
+//   console.log(store.getState());
+//
+//   return result;
+// };
+//
+// const applyMiddlewares = (store, middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach(mw => dispatch = mw(store)(dispatch));
+//   return Object.assign({}, store, { dispatch });
+// };
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+
+  // store = applyMiddlewares(store, [addLoggingToDispatch]);
+
+  // store.dispatch = addLoggingToDispatch(store);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
